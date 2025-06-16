c1c3019e30bcb783b40835ba9a1a46327ce9ed85
VisheshGupta01 <visheshgupta890@gmail.com>
2025-06-16 10:49:48 +0530

    changed position of task tracker in main dashboard

--- src/components/MainDashboard.tsx
+++ src/components/MainDashboard.tsx
@@ -29,6 +29,9 @@ const MainDashboard = ({ userIdea }: MainDashboardProps) => {
 
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+      <div className="h-64 border-t border-border bg-card">
+          <TaskTracker />
+        </div>
         {/* Top Section - Chat and Preview */}
         <div className="flex flex-1 min-h-0">
           {/* AI Chat Panel */}
@@ -43,9 +46,6 @@ const MainDashboard = ({ userIdea }: MainDashboardProps) => {
         </div>
 
         {/* Bottom Section - Task Tracker */}
-        <div className="h-64 border-t border-border bg-card">
-          <TaskTracker />
-        </div>
       </div>
     </div>
   );
